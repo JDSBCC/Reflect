c9953be2b9748dca517de3989fbbd84986cb9911
JDSBCC <ei12159@fe.up.pt>
2015-11-07 18:50:25 +0000

    Fixed position of the objects.
    Added HealthPoints.

---
+++ Assets/Scripts/BaseHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseHealth : MonoBehaviour {
+
+    public GameObject healthPoint;
+    private int health = 0;
+    private bool init = false;
+
+	// Use this for initialization
+	void Start (){
+    }
+
+    void drawHealthPoint(){
+        if (health != 10){
+            Instantiate(healthPoint, new Vector3(transform.position.x - 50, 100, transform.position.z - 50), Quaternion.identity);
+            health++;
+            Invoke("drawHealthPoint", 2.0f);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (GetComponentInChildren<MeshRenderer>().isVisible && !init){
+            drawHealthPoint();
+            init = true;
+        }
+    }
+}
--- Assets/Scripts/Position.cs
+++ Assets/Scripts/Position.cs
@@ -12,5 +12,6 @@ public class Position : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+        transform.position = new Vector3(transform.position.x, go.transform.position.y + transform.GetComponent<MeshRenderer>().bounds.size.y/2, transform.position.z);
     }
 }
