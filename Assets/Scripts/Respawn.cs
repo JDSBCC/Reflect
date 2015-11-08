259d54d610867e88b1ac981f6b2cc508ccfd408d
JDSBCC <ei12159@fe.up.pt>
2015-11-08 13:02:45 +0000

    Minios spawn initiated.

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -4,26 +4,44 @@ using System.Collections;
 public class Respawn : MonoBehaviour {
 
     public GameObject minion;
+    public GameObject minion_red;
+    public GameObject minion_green;
+    public GameObject minion_blue;
     private bool init = false;
+    private static int[] choices = {200, -200};
 
-	// Use this for initialization
-	void Start ()
+    public int minionCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    // Use this for initialization
+    void Start ()
     {
-
     }
 	
 	// Update is called once per frame
-	void Update ()
-    {
+	void Update (){
         //Debug.Log();
-        if (GetComponentInChildren<MeshRenderer>().isVisible && !init)
-        {
-            Instantiate(minion, new Vector3(-200, 0, -200), Quaternion.identity);
-            Instantiate(minion, new Vector3(200, 0, -200), Quaternion.identity);
-            Instantiate(minion, new Vector3(-200, 0, 200), Quaternion.identity);
-            Instantiate(minion, new Vector3(200, 0, 200), Quaternion.identity);
+        if (GetComponentInChildren<MeshRenderer>().isVisible && !init){
+            StartCoroutine(SpawnWaves());
             init = true;
         }
 
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < minionCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(choices[Random.Range(0, 2)], 0, choices[Random.Range(0, 2)]);
+                Instantiate(minion, spawnPosition, Quaternion.identity);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
     }
 }
